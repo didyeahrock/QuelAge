1b94c2774b2b013cb8b16a18c101672204b2b1e4
didyeahrock <didyeah.rock@gmail.com>
2021-09-14 18:21:16 +0200

    First commit

---
+++ age.py
@@ -0,0 +1,7 @@
+age = input("Quel age avez-vous ? ")
+age_int=int(age)
+if age_int < 18:
+	print("Vous avez",age_int,"ans, vous êtes donc mineur")
+else:
+	print("Vous avez",age_int,"ans, vous êtes donc majeur")
+
